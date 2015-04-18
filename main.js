706480c187285beda7e0a262800141465ef328fc
eric <theguy@eric.host22.com>
2015-04-18 01:20:04 -0400

    Minor revisions to game engine, tiny optimizations.

--- main.js
+++ main.js
@@ -314,7 +314,7 @@ var drawScore = function() {
 	ctx.textAlign = "left";
 	ctx.fillText("Score: " + score, 20, 50);
 	ctx.fillText("High Score: " + highScore, 20, 70);
-	ctx.fillText("FPS: " + game.fps, 20, 90);
+	ctx.fillText("FPS: " + game.fps.current, 20, 90);
 	ctx.fillText("ball.speed: " + ball.speed, 20, 110);
 	ctx.fillText("game.timePerTick: " + game.timePerTick, 20, 130);
 };
